39dff62ddf69183afc478ea18c4c446f8e72727b
BenTrapani <ben.trapani1995@gmail.com>
2018-03-11 08:20:44 -0400

    Fix artifacts resulting in out of order population of output from tc stage

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
   * GLM library: https://github.com/g-truc/glm
   * SDL2 CMake script: https://github.com/brendan-w/collector/blob/master/cmake/FindSDL2.cmake
   * Tesselation example: http://ogldev.atspace.co.uk/www/tutorial30/tutorial30.html
+  * Tesselation based on screen coordinates: http://codeflow.org/entries/2010/nov/07/opengl-4-tessellation/
 * (Optional) What was the most interesting part of the assignment? How would you improve this assignment?
   Seeing the result of loading the complex bunny model was really cool. Adding
   functionality for moving around the resulting scene would have been helpful in diagnosing issues with
--- shaders/TessControl.glsl
+++ shaders/TessControl.glsl
@@ -12,21 +12,20 @@ vec2 projectToScreen(vec3 value) {
     vec4 result = viewProj * vec4(value, 1.0);
     result /= result.w;
     vec2 resultInXYPlane = vec2(result);
-    return resultInXYPlane;
-    //resultInXYPlane += 1.0; //Now in range 0 to 2
-    //return resultInXYPlane * (screenSize * 0.5);
+    resultInXYPlane += 1.0; //Now in range 0 to 2
+    return resultInXYPlane * (screenSize * 0.5);
 }
 
 float computeTessFactor(vec2 screenV1, vec2 screenV2) {
-    return clamp(distance(screenV1, screenV2), 1, 32);
+    return clamp(distance(screenV1, screenV2) / 32.0, 1.0, 512.0);
 }
 
 void main() {
     TEworldPos[gl_InvocationID] = TCworldPos[gl_InvocationID];
 
-    vec2 vert0Screen = projectToScreen(TEworldPos[0]);
-    vec2 vert1Screen = projectToScreen(TEworldPos[1]);
-    vec2 vert2Screen = projectToScreen(TEworldPos[2]);
+    vec2 vert0Screen = projectToScreen(TCworldPos[0]);
+    vec2 vert1Screen = projectToScreen(TCworldPos[1]);
+    vec2 vert2Screen = projectToScreen(TCworldPos[2]);
 
     gl_TessLevelOuter[0] = computeTessFactor(vert1Screen, vert2Screen);
     gl_TessLevelOuter[1] = computeTessFactor(vert2Screen, vert0Screen);
